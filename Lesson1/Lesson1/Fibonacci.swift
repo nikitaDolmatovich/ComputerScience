006810719ede8dc3b3f31c32928dc1a205491523
Nikita <Nikita@MacBook-Air-User.local>
2017-02-07 22:05:38 +0300

    calculate with array commit

--- Lesson1/Lesson1/Fibonacci.swift
+++ Lesson1/Lesson1/Fibonacci.swift
@@ -7,13 +7,38 @@
 //
 
 class Fibonacci {
+    
+    private var array = [Int](repeatElement(0, count: 100))
+    private var cache = [Int]()
+    
     init() {}
     
-    func calcRecursion(itemCount : Int) -> Int {
-        if itemCount == 1 || itemCount == 2 {
+    public func calcRecursion(itemCount : Int) -> Int {
+        if testFirstOrSecond(itemCount: itemCount) {
             return 1
         } else {
             return calcRecursion(itemCount: itemCount - 1) + calcRecursion(itemCount: itemCount - 2)
         }
     }
+    
+    public func calcWithArray(itemCount : Int) -> CLongLong {
+        if array[itemCount] == 0 {
+            if testFirstOrSecond(itemCount: itemCount) {
+                array[itemCount] = 1
+            } else {
+                array[itemCount] = Int(calcWithArray(itemCount: itemCount - 2) + calcWithArray(itemCount: itemCount - 1))
+            }
+        }
+        
+        return CLongLong(array[itemCount])
+    }
+    
+    private func testFirstOrSecond(itemCount : Int) -> Bool {
+        if itemCount == 1 || itemCount == 2 {
+            return true
+        } else {
+            return false
+        }
+    }
+    
 }
