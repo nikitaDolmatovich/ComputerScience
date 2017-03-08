01482313bda5db5955ad80d27ce092a5a74fefb1
dolma <dolma@DESKTOP-RNFRDOG>
2017-03-08 17:48:30 +0300

    Add class InternalNode commit

---
+++ Lesson3/Lesson3/TaskA/InternalNode.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson3.TaskA
+{
+    public class InternalNode : Node
+    {
+        private Node left;
+        private Node right;
+
+        public InternalNode(Node left, Node right) : base(left.Frequency + right.Frequency)
+        {
+            this.left = left;
+            this.right = right;
+        }
+
+        public override void FillCodes(string code)
+        {
+            left.FillCodes(code + "0");
+            right.FillCodes(code + "1");
+        }
+    }
+}
