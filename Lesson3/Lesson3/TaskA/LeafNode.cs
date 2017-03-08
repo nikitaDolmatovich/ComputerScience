80ab4c05c77816672b9586f80597650e6d00175e
dolma <dolma@DESKTOP-RNFRDOG>
2017-03-08 17:53:33 +0300

    Add class LeafNode commit

---
+++ Lesson3/Lesson3/TaskA/LeafNode.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson3.TaskA
+{
+    public class LeafNode : Node
+    {
+        private char symbol;
+        private HoffmanA hoff = new HoffmanA();
+
+        public LeafNode(char symbol, int frequency) : base(frequency)
+        {
+            this.symbol = symbol;
+        }
+
+        public override void FillCodes(string code)
+        {
+            hoff.Codes.Add(this.symbol, code);
+        }
+    }
+}
