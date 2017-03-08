e3582b842f237f893d94c5c01dfa03a7953557b0
dolma <dolma@DESKTOP-RNFRDOG>
2017-03-08 17:43:46 +0300

    Add abstract class Node commit

---
+++ Lesson3/Lesson3/TaskA/Node.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson3.TaskA
+{
+    public abstract class Node : IComparable<Node>
+    {
+        public int Frequency { get; set; }
+
+        public Node(int frequency)
+        {
+            Frequency = frequency;
+        }
+
+        public abstract void FillCodes(string code);
+
+        public int CompareTo(Node node)
+        {
+            if (Frequency > node.Frequency) return 1;
+            else if (Frequency == node.Frequency) return 0;
+
+            return -1;
+        }
+    }
+}
