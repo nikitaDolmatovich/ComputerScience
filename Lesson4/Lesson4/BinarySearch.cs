31ec7010bedc709c9debda91c29c648aa618662c
dolma <dolma@DESKTOP-RNFRDOG>
2017-03-28 22:29:38 +0300

    Binary Search done commit

---
+++ Lesson4/Lesson4/BinarySearch.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson4
+{
+    public static class BinarySearch
+    {
+        public static int? BinarySearchs(int[] a, int x)
+        {
+            if ((a.Length == 0) || (x < a[0]) || (x > a[a.Length - 1]))
+            {
+                return null;
+            }
+
+            int first = 0;
+      
+            int last = a.Length;
+
+            while (first < last)
+            {
+                int mid = first + (last - first) / 2;
+
+                if (x <= a[mid])
+                    last = mid;
+                else
+                    first = mid + 1;
+            }
+
+            if (a[last] == x)
+                return last;
+            else
+                return null;
+        }
+    }
+}
