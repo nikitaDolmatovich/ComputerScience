edba015506c15e83c408d3bbc7d21365b6d7a592
dolma <dolma@DESKTOP-RNFRDOG>
2017-05-10 12:47:49 +0300

    Task B done commit

---
+++ Lesson10/Lesson10/TopologicSearch.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson10
+{
+    public static class TopologicSearch
+    {
+        static void dfs(List<int> graph, bool[] used, List<int> res, int u)
+        {
+            used[u] = true;
+            foreach (var v in graph[u])
+                if (!used[v])
+                    dfs(graph, used, res, v);
+            res.add(u);
+        }
+
+        public static List<int> topologicalSort(List<int>[] graph)
+        {
+            int n = graph.Length;
+            bool[] used = new bool[n];
+            List<int> res = new List<int>();
+            for (int i = 0; i < n; i++)
+            {
+                if (!used[i])
+                {
+                    dfs(graph, used, res, i);
+                }
+            }
+            Array.Reverse(res);
+            return res;
+        }
+    }
+}
