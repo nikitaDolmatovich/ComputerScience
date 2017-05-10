115834b54dc9b57c991c39573f405f0d5d3fbe39
dolma <dolma@DESKTOP-RNFRDOG>
2017-05-10 17:07:24 +0300

    Task B done commit

---
+++ Lesson11/Lesson11/Bellman_Ford.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson11
+{
+    public class Bellman_Ford
+    {
+        private static int INF = int.MaxValue / 2;
+
+        private int numberVertex;
+        private int numberEdge;
+        private Edge[] edges;
+        private bool[]used;
+        private int[] distance;
+
+        private int[] parent;
+        int start;
+
+        private void dejkstra(int s)
+        {
+            distance[s] = 0;
+            for (int i = 0; i < numberVertex - 1; i++)
+            {
+                for (int j = 0; j < numberEdge; j++)
+                {
+                    int from = edges[j].GetFrom();
+                    int to = edges[j].GetTo();
+                    int weight = edges[j].GetWeight();
+
+                    
+                    if (distance[from] == INF)
+                    {
+                        continue;
+                    }
+
+                  
+                    distance[to] = Math.Min(distance[to], distance[from] + weight);
+                    parent[to] = from;
+                }
+            }
+        }
+
+        private void PrintData()
+        {
+  	        for (int v = 0; v<numberVertex; ++v) {
+  	            if (distance[v] != INF) {
+  		            Console.Write(distance[v] + " ");
+                } else {
+  		            Console.Write("-1 ");
+                }
+  	        }
+        }
+    }
+}
---
+++ Lesson11/Lesson11/Edge.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson11
+{
+    public class Edge
+    {
+        int from;
+        int to;
+        int weight;
+
+        public Edge(int u, int v, int w)
+        {
+            this.from = u;
+            this.to = v;
+            this.weight = w;
+        }
+
+        public int GetFrom()
+        {
+            return from;
+        }
+
+        public void SetFrom(int from)
+        {
+            this.from = from;
+        }
+
+        public int GetTo()
+        {
+            return to;
+        }
+
+        public void SetTo(int to)
+        {
+            this.to = to;
+        }
+
+        public int GetWeight()
+        {
+            return weight;
+        }
+
+        public void SetWeight(int weight)
+        {
+            this.weight = weight;
+        }
+
+    }
+}
